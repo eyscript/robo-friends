4524bde36542f0eba0d2e5d025498abfb5aacaab
eyscript <52507973+eyscript@users.noreply.github.com>
2020-08-19 14:48:18 -0500

    Updates

---
+++ precache-manifest.4d9a33bf3d2da0dcfb1b2fd473af5379.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed72de2db370fb825a5e5e18cc9a8f06",
+    "url": "/robo-friends/index.html"
+  },
+  {
+    "revision": "83223d7168ceef33dc8c",
+    "url": "/robo-friends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b20ead5e259ed1e1c648",
+    "url": "/robo-friends/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "83223d7168ceef33dc8c",
+    "url": "/robo-friends/static/js/2.653299bc.chunk.js"
+  },
+  {
+    "revision": "23900771a863482f56e68d68cf1f3072",
+    "url": "/robo-friends/static/js/2.653299bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b20ead5e259ed1e1c648",
+    "url": "/robo-friends/static/js/main.e45976a4.chunk.js"
+  },
+  {
+    "revision": "b7276070548a11fb737e",
+    "url": "/robo-friends/static/js/runtime-main.17f36485.js"
+  }
+]);
